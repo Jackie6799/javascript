e28e5f0b8ed47105d8eeb97ef0a19becaac38657
Salaiman Darman <salaiman@salaimans-mini.home>
2019-08-17 19:01:07 +1000

    p5 changes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function myFunction() {
   alert("I am an alert box!");
 }
+
+let circle = 50;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(250, 100, 100);
+  fill(250, 200, 200);
+  ellipse(circle, 200, 55, 55);
+  circle = circle + 2;
+}
