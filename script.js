fe0c793c87b38791683aec8bf931aab5c77d4140
Salaiman Darman <salaiman@salaimans-mini.home>
2019-08-17 19:11:20 +1000

    fix file

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function myFunction() {
   alert("I am an alert box!");
 }
 
-<<<<<<< HEAD
 let circle = 50;
 
 function setup() {
@@ -15,8 +14,3 @@ function draw() {
   ellipse(circle, 200, 55, 55);
   circle = circle + 2;
 }
-=======
-document.write(Date());
-
-document.write("Hello SD!");
->>>>>>> ed0e2f92e28c618f285049c820d15d05afd19f37
