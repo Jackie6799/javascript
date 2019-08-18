c50902ef79896a797e59cb657debe20d90a5372b
salaiman <smdarman@yahoo.com>
2019-08-18 19:45:42 +1000

    created object with random

--- script.js
+++ script.js
@@ -2,28 +2,26 @@ function myFunction() {
   alert("I am an alert box!");
 }
 
-let circle = 50;
-
-let circle={
-    x = 30;
-    y = 200;
-    width = 85;
-    length = 85;
 
 
+let circle = {
+    x: 30,
+    y: 50,
+    width: 85,
 
-}
-
+};
 
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
-  background(250, 100, 100);
+  background(250, 400, 100);
   fill(250, 200, 200);
-  ellipse(circle, 200, 85, 85);
-  circle = circle + 5;
+  circle.x = random(0,600);
+  circle.y = random(0,600);
+  ellipse(circle.x, circle.y, circle.width, circle.width);
+  circle.x = circle.x + 5;
 }
 function date() {
   document.write(Date());
