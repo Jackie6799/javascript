ea6fa7f0636f34abdd8f50d57c539e8069137377
salaiman <smdarman@yahoo.com>
2019-08-29 20:35:13 +1000

    new files

---
+++ random.js
@@ -0,0 +1,13 @@
+
+let choice = {
+   x: 30,
+   y: 50,
+   width: 85
+}
+
+
+function mousepressed() {
+
+  choice.x = random (1,3);
+  print(choice.x);
+}
--- script.js
+++ script.js
@@ -3,35 +3,33 @@ function myFunction() {
 }
 
 
+//
+//let circle = {
+//    x: 30,
+//    y: 50,
+//    width: 85,
+//
+//};
+//
+//let speed = 5;
+//
+//function setup() {
+//  createCanvas(800, 800);
+//}
+//
+//function draw() {
+//  background(250, 400, 100);
+//  fill(250, 200, 200);
+////  circle.x = random(0,600);
+////  circle.y = random(0,600);
+//  ellipse(circle.x, circle.y, circle.width, circle.width);
+//
+//  if (circle.x > width) {
+//
+//        speed = -5;
+//
+//                     };
+//  circle.x = circle.x + speed;
+//}
 
-let circle = {
-    x: 30,
-    y: 50,
-    width: 85,
-
-};
-
-let speed = 5;
-
-function setup() {
-  createCanvas(800, 800);
-}
-
-function draw() {
-  background(250, 400, 100);
-  fill(250, 200, 200);
-//  circle.x = random(0,600);
-//  circle.y = random(0,600);
-  ellipse(circle.x, circle.y, circle.width, circle.width);
-
-  if (circle.x > width) {
-
-        speed = -5;
-
-                     };
-  circle.x = circle.x + speed;
-}
-function date() {
-  document.write(Date());
-}
 
